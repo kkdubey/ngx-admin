83e295dc0d8503b9a607aab02ffd4eb2371a2fe1
komal dubey <kkdubey1989@gmail.com>
2018-03-27 16:13:25 +0530

    code refactor

--- src/app/pages/components/components-routing.module.ts
+++ src/app/pages/components/components-routing.module.ts
@@ -2,17 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ComponentsComponent } from './components.component';
-import { TreeComponent } from './tree/tree.component';
 import { NotificationsComponent } from './notifications/notifications.component';
 
 const routes: Routes = [{
   path: '',
   component: ComponentsComponent,
-  children: [
-  {
-    path: 'tree',
-    component: TreeComponent,
-  }, {
+  children: [ {
     path: 'notifications',
     component: NotificationsComponent,
   },
@@ -27,6 +22,5 @@ export class ComponentsRoutingModule { }
 
 export const routedComponents = [
   ComponentsComponent,
-  TreeComponent,
   NotificationsComponent,
 ];
--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -2,29 +2,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 import { PagesComponent } from './pages.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [{
   path: '',
   component: PagesComponent,
   children: [{
-    path: 'dashboard',
-    component: DashboardComponent,
-  }, {
     path: 'issue-tracker',
     loadChildren: './ui-features/ui-features.module#UiFeaturesModule',
-  }, {
-    path: 'components',
-    loadChildren: './components/components.module#ComponentsModule',
-  }, {
-    path: 'charts',
-    loadChildren: './charts/charts.module#ChartsModule',
-  }, {
-    path: 'editors',
-    loadChildren: './editors/editors.module#EditorsModule',
-  },  {
-    path: 'tables',
-    loadChildren: './tables/tables.module#TablesModule',
   }, {
     path: '',
     redirectTo: 'issue-tracker',
